36a7d10fecc8930fdf5ba67049ddc20d25d778b9
kumarsurajverma6001@gmail.com <kumarsurajverma6001@gmail.com>
2023-11-04 22:02:25 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,60 +1,68 @@
 //! Import required modules
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-require('dotenv').config();
-const path = require('path');
-const methodOverride = require('method-override');
-const ejsMate = require('ejs-mate');
+const mongoose = require("mongoose");
+require("dotenv").config();
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
-const session = require('express-session');
-const flash = require('connect-flash');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./model/user');
+const session = require("express-session");
+const mongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./model/user");
 
-const listingRouter = require('./routes/listing');
-const reviewRouter = require('./routes/review');
-const userRouter = require('./routes/user');
-
+const listingRouter = require("./routes/listing");
+const reviewRouter = require("./routes/review");
+const userRouter = require("./routes/user");
+const MongoStore = require("connect-mongo");
+const { error } = require("console");
 
 //! Get the MongoDB URI from environment variables
+const port = process.env.PORT;
 const MONGODB_URL = process.env.MONGODB_URI;
-
 
 //! Function to connect to the database
 async function main() {
-    try {
-        await mongoose.connect(MONGODB_URL);
-        console.log("Database Connected");
-    } catch (err) {
-        console.error("Error connecting to the database:", err);
-    }
+  try {
+    await mongoose.connect(MONGODB_URL);
+    console.log("Database Connected");
+  } catch (err) {
+    console.error("Error connecting to the database:", err);
+  }
 }
 
 //! Call the main function to connect to the database
-main()
+main();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.engine('ejs', ejsMate);
-
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 //! app.use(express.static, path.join(__dirname, '/public'));
 app.use(express.static(path.join(__dirname, "/public")));
 
-// //! Define a route for the root path ('/') and send a simple response
-// app.get('/', (req, res) => {
-//     res.send("Hey, I'm root");
-// });
+const store = MongoStore.create({
+  mongoUrl: MONGODB_URL,
+  crypto: {
+    secret: "mysupersecretcode",
+  },
+  touchAfter: 24 * 3600,
+});
 
+store.on("error", () => {
+  console.log("Error In MngoDB", error);
+});
 
 const sessionOption = {
-    secret: 'mysupersecretcode',
-    resave: false,
-    saveUninitialized: true
+  store,
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
 };
 
 app.use(session(sessionOption));
@@ -67,39 +75,27 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    next();
-})
-
-// app.get('/demouser', async (req, res) => {
-//     let fakeUser = new User({
-//         email: 'admin@gmail.com',
-//         username: 'suraj789'
-//     });
-//     let registeredUser = await User.register(fakeUser, 'Suraj789');
-//     res.send(registeredUser);
-// })
-
-app.use('/listings', listingRouter);
-app.use('/listings/:id/reviews', reviewRouter);
-app.use('/', userRouter)
-
-
-app.all('*', (req, res, next) => {
-    res.render('pageNotFound.ejs');
-})
-
-
-app.use((err, req, res, next) => {
-    console.error(err); // Log the error to the console for debugging
-    const statusCode = err.statusCode || 500;
-    res.render('error.ejs');
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
 });
 
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
+
+app.all("*", (req, res, next) => {
+  res.render("pageNotFound.ejs");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err); // Log the error to the console for debugging
+  const statusCode = err.statusCode || 500;
+  res.render("error.ejs");
+});
 
 //! Start the Express app and listen on port 8080
-app.listen(8080, () => {
-    console.log("Server is listening on port number 8080");
+app.listen(port, () => {
+  console.log(`Server is listening on port number ${port}`);
 });
